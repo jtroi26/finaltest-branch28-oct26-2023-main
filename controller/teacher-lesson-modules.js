0827e82132266464efcdc250661b0a24b73c239c
illuminxry <secret.ian0926@gmail.com>
2023-11-02 16:36:38 +0800

    teacher-lesson-modules now working

--- controller/teacher-lesson-modules.js
+++ controller/teacher-lesson-modules.js
@@ -13,13 +13,13 @@ const conn = {
 //     const subjectname = req.session.subjectname;
 //     const sectionname = req.session.sectionname;
 
-//     const values = [sectionname, subjectname];
 
 
-//     const firstquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'First QUARTER' AND sectionname = '?' AND subjectname = '?';`
-//     const secondquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Second Quarter AND sectionname = '?' AND subjectname = '?';`
-//     const thirdquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Third Quarter AND sectionname = '?' AND subjectname = '?';`
-//     const fourthquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Fourth Quarter AND sectionname = '?' AND subjectname = '?';`
+
+//     const firstquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'First QUARTER' AND sectionname = '?' AND subjectname = '?' ORDER BY lessonID ASC;`
+//     const secondquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Second Quarter' AND sectionname = '?' AND subjectname = '?' ORDER BY lessonID ASC;`
+//     const thirdquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Third Quarter' AND sectionname = '?' AND subjectname = '?' ORDER BY lessonID ASC;`
+//     const fourthquartersql = `SELECT id, lessonID, lessonTitle FROM lessons WHERE quarterperiod = 'Fourth Quarter' AND sectionname = '?' AND subjectname = '?' ORDER BY lessonID ASC;`
     
 //     res.render('teacher-lesson-modules',{ subjectid, subjectname, sectionname});
 // };
@@ -30,85 +30,55 @@ exports.getSubjectModules = (req, res) => {
     const subjectname = req.session.subjectname;
     const sectionname = req.session.sectionname;
 
+    // Define placeholders for sectionname and subjectname
     const values = [sectionname, subjectname];
 
-    const firstquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'First Quarter' AND sectionname = ? AND subjectname = ?`;
-    const secondquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'Second Quarter' AND sectionname = ? AND subjectname = ?`;
-    const thirdquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'Third Quarter' AND sectionname = ? AND subjectname = ?`;
-    const fourthquartersql = `SELECT id, lessonID, lessonTitle, sectionname, subjectname FROM lessons WHERE quarterperiod = 'Fourth Quarter' AND sectionname = ? AND subjectname = ?`;
+    const firstquartersql = `SELECT id, lessonID, lessonTitle, quarterperiod, subjectname FROM lessons WHERE quarterperiod = 'First Quarter' AND sectionname = ? AND subjectname = ? ORDER BY lessonID ASC`;
+    const secondquartersql = `SELECT id, lessonID, lessonTitle, quarterperiod, subjectname FROM lessons WHERE quarterperiod = 'Second Quarter' AND sectionname = ? AND subjectname = ? ORDER BY lessonID ASC`;
+    const thirdquartersql = `SELECT id, lessonID, lessonTitle, quarterperiod, subjectname FROM lessons WHERE quarterperiod = 'Third Quarter' AND sectionname = ? AND subjectname = ? ORDER BY lessonID ASC`;
+    const fourthquartersql = `SELECT id, lessonID, lessonTitle, quarterperiod, subjectname FROM lessons WHERE quarterperiod = 'Fourth Quarter' AND sectionname = ? AND subjectname = ? ORDER BY lessonID ASC`;
 
-    const renderData = {
-        subjectid,
-        subjectname,
-        sectionname,
-    };
-
+    // Execute SQL queries
     connection.query(firstquartersql, values, (err, firstQuarterResults) => {
         if (err) {
             console.error('Error fetching first quarter lessons:', err);
-            // Handle the error (e.g., send an error response)
+            connection.end();
             res.status(500).send('Internal Server Error');
-        } else if (firstQuarterResults.length === 0) {
-            renderData.firstQuarterMessage = 'No lesson was created for the first quarter.';
-            renderSecondQuarter();
         } else {
-            renderData.firstQuarterLessons = firstQuarterResults;
-            renderSecondQuarter();
-        }
-    });
-
-    function renderSecondQuarter() {
-        connection.query(secondquartersql, values, (err, secondQuarterResults) => {
-            if (err) {
-                console.error('Error fetching second quarter lessons:', err);
-                // Handle the error (e.g., send an error response)
-                res.status(500).send('Internal Server Error');
-            } else if (secondQuarterResults.length === 0) {
-                renderData.secondQuarterMessage = 'No lesson was created for the second quarter.';
-                renderThirdQuarter();
-            } else {
-                renderData.secondQuarterLessons = secondQuarterResults;
-                renderThirdQuarter();
-            }
-        });
-    }
-
-    function renderThirdQuarter() {
-        connection.query(thirdquartersql, values, (err, thirdQuarterResults) => {
-            if (err) {
-                console.error('Error fetching third quarter lessons:', err);
-                // Handle the error (e.g., send an error response)
-                res.status(500).send('Internal Server Error');
-            } else if (thirdQuarterResults.length === 0) {
-                renderData.thirdQuarterMessage = 'No lesson was created for the third quarter.';
-                renderFourthQuarter();
-            } else {
-                renderData.thirdQuarterLessons = thirdQuarterResults;
-                renderFourthQuarter();
-            }
-        });
-    }
-
-    function renderFourthQuarter() {
-        connection.query(fourthquartersql, values, (err, fourthQuarterResults) => {
-            if (err) {
-                console.error('Error fetching fourth quarter lessons:', err);
-                // Handle the error (e.g., send an error response)
-                res.status(500).send('Internal Server Error');
-            } else if (fourthQuarterResults.length === 0) {
-                renderData.fourthQuarterMessage = 'No lesson was created for the fourth quarter.';
-            } else {
-                renderData.fourthQuarterLessons = fourthQuarterResults;
-            }
-
-            // Close the database connection
-            connection.end((err) => {
+            connection.query(secondquartersql, values, (err, secondQuarterResults) => {
                 if (err) {
-                    console.error('Error closing the database connection:', err);
+                    console.error('Error fetching second quarter lessons:', err);
+                    connection.end();
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    connection.query(thirdquartersql, values, (err, thirdQuarterResults) => {
+                        if (err) {
+                            console.error('Error fetching third quarter lessons:', err);
+                            connection.end();
+                            res.status(500).send('Internal Server Error');
+                        } else {
+                            connection.query(fourthquartersql, values, (err, fourthQuarterResults) => {
+                                connection.end(); // Close the database connection
+                                if (err) {
+                                    console.error('Error fetching fourth quarter lessons:', err);
+                                    res.status(500).send('Internal Server Error');
+                                } else {
+                                    res.render('teacher-lesson-modules', {
+                                        subjectid,
+                                        subjectname,
+                                        sectionname,
+                                        firstQuarterLessons: firstQuarterResults,
+                                        secondQuarterLessons: secondQuarterResults,
+                                        thirdQuarterLessons: thirdQuarterResults,
+                                        fourthQuarterLessons: fourthQuarterResults,
+                                    });
+                                }
+                            });
+                        }
+                    });
                 }
             });
-
-            res.render('teacher-lesson-modules', renderData);
-        });
-    }
+        }
+    });
 };
+
--- readme.md
+++ readme.md
@@ -48,3 +48,6 @@ branch 41 - delete department function
 branch 42 - updated edit function for teacher account details, suffix view in index-teacher and view-teacher-account
 branch 43 - view subject for teacher page created
 branch 44 - working session teacher lesson module
+branch 45 -
+branch 46
+branch 47 - lesson module view now working
