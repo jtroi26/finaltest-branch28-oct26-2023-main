625f09f62958fe55dd2c8f0cbe70d46826fed1c3
illuminxry <secret.ian0926@gmail.com>
2023-11-02 13:23:16 +0800

    removed whitespace

--- controller/teacher-lesson-modules.js
+++ controller/teacher-lesson-modules.js
@@ -12,7 +12,7 @@ exports.getSubjectModules = (req, res) => {
     const subjectid = req.session.subjectid;
     const subjectname = req.session.subjectname;
     const sectionname = req.session.sectionname;
-    // console.log(subjectid);
+
     
     res.render('teacher-lesson-modules',{ subjectid, subjectname, sectionname});
 };
