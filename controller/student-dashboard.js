d4b7190fb4a74fe129b45e95c8005a6414dde973
rmsmrcc <rmsmrcc@gmail.com>
2023-11-11 23:30:04 +0800

    new changes

--- controller/student-dashboard.js
+++ controller/student-dashboard.js
@@ -23,24 +23,41 @@ exports.getStudentDashboard = (req, res) => {
     ON subjects.sectionname = students.sectionname
     WHERE students.studentID = ? AND students.sectionname = ? AND subjects.visibility = 'Visible'`;
 
+    const sqlannouncement = `SELECT id, teacherid, subjectname, sectionname, announcementTitle, announcement, visibility, dateCreated from teacherannouncements WHERE  sectionname = ? ORDER BY dateCreated DESC LIMIT 5`
+    
     const studentid = req.session.studentID;
     const sectionname = req.session.sectionname;
-
+    const announcementvalues = [sectionname];
     const values = [studentid, sectionname];
 
-    connection.query(sql, values, (error, results) => {
+    connection.query(sql, values, (error, studentResults) => {
         if (error) {
             console.error(error);
+            connection.end();
+            res.status(500).send('Internal Server Error');
         } else {
-            console.log(results);
-            res.render('student-dashboard', {
-                studentfullname,
-                studentfirstname: req.session.studentfirstname,
-                studentid,
-                studentData: results, // Pass the entire results array
+            // Execute the second query after the first one
+            connection.query(sqlannouncement, announcementvalues, (announcementError, announcementResults) => {
+                if (announcementError) {
+                    console.error(announcementError);
+                    connection.end();
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    console.log(studentResults);
+                    console.log(announcementResults);
+                    res.render('student-dashboard', {
+                        studentfullname,
+                        studentfirstname: req.session.studentfirstname,
+                        studentid,
+                        studentData: studentResults, // Pass the results of the first query
+                        announcementData: announcementResults, // Pass the results of the second query
+                    });
+
+                    // Close the database connection after rendering the template
+                    connection.end();
+                }
             });
         }
     });
 };
-
 
