34eb9383c5cacfdc16da8f00100f5cb333a6eb5a
illuminxry <secret.ian0926@gmail.com>
2023-11-12 11:13:14 +0800

    edited dateEnrolled

--- controller/admin-index-student.js
+++ controller/admin-index-student.js
@@ -8,70 +8,51 @@ const conn = {
 };
 
 exports.getIndexStudents = (req, res) => {
-    const sql = `
-      SELECT
-        id,
-        studentID,
-        firstname,
-        middlename,
-        lastname,
-        CONCAT(firstname, ' ', middlename, ' ', lastname, 
-          CASE WHEN suffix = 'none' THEN ' ' ELSE suffix END
-        ) AS 'Student Name',
-        sectionname,
-        dateEnrolled,
-        status
-      FROM students
-      ORDER BY studentID DESC, status ASC
-    `;
-    const sectionsql = `SELECT sectionname FROM sections WHERE visibility = 'Visible'`;
-    const connection = mysql.createConnection(conn);
-  
-    // connection.query(sql, (error, results) => {
-    //   if (error) {
-    //     // Handle the error
-    //     console.error(error);
-    //   } else {
-    //     // Render the EJS template and pass the results
-    //     res.render('admin-index-student', { data: results, admin_id: req.session.admin_id });
-    //     console.log(results);
-    //   }
-    //   connection.end(); // Close the MySQL connection when done
-    // });
-    connection.query(sql, (err, results) => {
-      if (err) {
-          console.error('Error:', err);
-          connection.end(); // Close the database connection in case of an error
-          res.status(500).send('Internal Server Error');
-          return;
-      }
+  const sql = `
+    SELECT
+      id,
+      studentID,
+      firstname,
+      middlename,
+      lastname,
+      CONCAT(firstname, ' ', middlename, ' ', lastname,' ', 
+        CASE WHEN suffix = 'none' THEN ' ' ELSE suffix END
+      ) AS 'Student Name',
+      sectionname,
+      dateEnrolled,
+      status
+    FROM students
+    ORDER BY studentID DESC, status ASC
+  `;
+  const sectionsql = `SELECT sectionname FROM sections WHERE visibility = 'Visible'`;
+  const connection = mysql.createConnection(conn);
 
-      // Execute the teachersql query
-      connection.query(sql, (err, results) => {
-          if (err) {
-              console.error('Error:', err);
-              connection.end(); // Close the database connection in case of an error
-              res.status(500).send('Internal Server Error');
-              return;
-          }
-          connection.query(sectionsql, (err, sectionResults) => {
-              if (err) {
-                  console.error('Error:', err);
-                  connection.end(); // Close the database connection in case of an error
-                  res.status(500).send('Internal Server Error');
-                  return;
-              }
+  connection.query(sql, (err, results) => {
+    if (err) {
+        console.error('Error:', err);
+        connection.end();
+        res.status(500).send('Internal Server Error');
+        return;
+    }
 
-              connection.end(); // Close the database connection
+    // Format dateEnrolled field
+    results.forEach(result => {
+        result.dateEnrolled = new Date(result.dateEnrolled).toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+        });
+    });
 
-              // Pass the data to your EJS template and render it
-              res.render('admin-index-student', { data: results, section:sectionResults, admin_id: req.session.admin_id });
-
-          });
-      });
-  });
-  };
-  
-
-  
-    
+    connection.query(sectionsql, (err, sectionResults) => {
+        if (err) {
+            console.error('Error:', err);
+            connection.end();
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        connection.end();
+        res.render('admin-index-student', { data: results, section: sectionResults, admin_id: req.session.admin_id });
+    });
+});
+};
