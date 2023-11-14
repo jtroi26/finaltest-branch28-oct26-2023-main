97ac4bb7bc46e9d5aa869802b811f3f27b9349dc
illuminxry <secret.ian0926@gmail.com>
2023-11-15 02:00:27 +0800

    optimized 15%

--- controller/student-view-lesson.js
+++ controller/student-view-lesson.js
@@ -36,10 +36,74 @@ exports.getLessonPage = (req, res) => {
     });
 };
 
+// At the beginning of your file or where appropriate, initialize a cache
+const responseCache = new Map();
+
+// exports.postOpenAI = async (req, res) => {
+//     const userInput = req.body.userInput || 'Hi! How can I help you today?';
+//     const relatedTopics = ['araling panlipunan', 'world history']; // Keywords related to the topics
+
+//     // Check if the user input matches specific keywords related to Araling Panlipunan or World History
+//     const isRelatedTopic = relatedTopics.some(topic => userInput.toLowerCase().includes(topic));
+
+//     if (!isRelatedTopic) {
+//         return res.json({ message: 'Please ask a question related to Araling Panlipunan or World History.' });
+//     }
+
+//         // Check if the response for the user input exists in the cache
+//         if (responseCache.has(userInput)) {
+//             const cachedResponse = responseCache.get(userInput);
+//             // Return the cached response immediately
+//             return res.json({ message: 'Cached response', data: cachedResponse });
+//         }
+
+//         const response = await openai.chat.completions.create({
+//             model: 'gpt-3.5-turbo',
+//             messages: [
+//                 { role: 'system', content: 'You are DAZSMA.AI, a kind and friendly chatbot.' },
+//                 { role: 'user', content: userInput }
+//             ],
+//         });
+
+//         // Cache the response for future use
+//         responseCache.set(userInput, response.choices[0].message.content);
+
+//         // Update the chat history in the session
+//         req.session.chatHistory = [
+//             ...(req.session.chatHistory || []),
+//             { role: 'user', content: userInput },
+//             { role: 'bot', content: response.choices[0].message.content }
+//         ];
+
+//         console.log(req.session.chatHistory);
+//         res.json({ message: 'Successfully submitted the form', data: response.choices[0].message.content });
+//     } catch (error) {
+//         console.error('Error fetching chat response:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// };
+
+// 
 exports.postOpenAI = async (req, res) => {
     try {
         const userInput = req.body.userInput || 'Hi! How can I help you today?';
+        const relatedTopics = ['araling panlipunan', 'world history']; // Keywords related to the topics
 
+        // Check if the user input matches specific keywords related to Araling Panlipunan or World History
+        const isRelatedTopic = relatedTopics.some(topic => userInput.toLowerCase().includes(topic));
+
+        if (!isRelatedTopic) {
+            return res.json({ message: 'Please ask a question related to Araling Panlipunan or World History.' });
+        }
+
+        // Check if the response for the user input exists in the cache
+        if (responseCache.has(userInput)) {
+            const cachedResponse = responseCache.get(userInput);
+            // Return the cached response immediately
+            return res.json({ message: 'Cached response', data: cachedResponse });
+        }
+
+        // Call the OpenAI API and process the response
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [
@@ -48,6 +112,9 @@ exports.postOpenAI = async (req, res) => {
             ],
         });
 
+        // Cache the response for future use
+        responseCache.set(userInput, response.choices[0].message.content);
+
         // Update the chat history in the session
         req.session.chatHistory = [
             ...(req.session.chatHistory || []),
@@ -55,9 +122,57 @@ exports.postOpenAI = async (req, res) => {
             { role: 'bot', content: response.choices[0].message.content }
         ];
 
-        res.json({ message: 'Successfully submitted the form' });
+        console.log(req.session.chatHistory);
+        res.json({ message: 'Successfully submitted the form', data: response.choices[0].message.content });
     } catch (error) {
         console.error('Error fetching chat response:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+};
+
+// exports.postOpenAI = async (req, res) => {
+//     try {
+//         const userInput = req.body.userInput || 'Hi! How can I help you today?';
+//         const relatedTopics = ['araling panlipunan', 'world history']; // Keywords related to the topics
+
+//         // Check if the user input matches specific keywords related to Araling Panlipunan or World History
+//         const isRelatedTopic = relatedTopics.some(topic => userInput.toLowerCase().includes(topic));
+
+//         if (!isRelatedTopic) {
+//             return res.json({ message: 'Please ask a question related to Araling Panlipunan or World History.' });
+//         }
+
+//         // Check if the response for the user input exists in the cache
+//         if (responseCache.has(userInput)) {
+//             const cachedResponse = responseCache.get(userInput);
+//             // Return the cached response immediately
+//             return res.json({ message: 'Cached response', data: cachedResponse });
+//         }
+
+//         // Call the OpenAI API and process the response
+//         const response = await openai.chat.completions.create({
+//             model: 'gpt-3.5-turbo',
+//             messages: [
+//                 { role: 'system', content: 'You are DAZSMA.AI, a kind and friendly chatbot.' },
+//                 { role: 'user', content: userInput }
+//             ],
+//         });
+
+//         // Cache the response for future use
+//         responseCache.set(userInput, response.choices[0].message.content);
+
+//         // Update the chat history in the session
+//         req.session.chatHistory = [
+//             ...(req.session.chatHistory || []),
+//             { role: 'user', content: userInput },
+//             { role: 'bot', content: response.choices[0].message.content }
+//         ];
+
+//         console.log(req.session.chatHistory);
+//         res.json({ message: 'Successfully submitted the form', data: response.choices[0].message.content });
+//     } catch (error) {
+//         console.error('Error fetching chat response:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// };
+
