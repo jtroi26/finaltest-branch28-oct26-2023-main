b8e09bab47a9c8c5097d98a0f83b02507c47975a
Illuminxry <secret.ian0926@gmail.com>
2024-04-27 14:35:54 +0800

    new empty db

---
+++ finaldb.sql
@@ -0,0 +1,661 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2024 at 08:10 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `finaldb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admindetails`
+--
+
+CREATE TABLE `admindetails` (
+  `id` int(11) NOT NULL,
+  `admin_id` varchar(25) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `middlename` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `visibility` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admindetails`
+--
+
+INSERT INTO `admindetails` (`id`, `admin_id`, `firstname`, `middlename`, `lastname`, `department`, `role`, `visibility`) VALUES
+(1, 'admin-10001', 'Christian Emmanuel', 'Avecilla', 'Pastrana', 'Araling Panlipunan', 'Admin', 'Visible');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogins`
+--
+
+CREATE TABLE `adminlogins` (
+  `id` int(11) NOT NULL,
+  `admin_id` varchar(25) DEFAULT NULL,
+  `username` varchar(25) NOT NULL,
+  `userpassword` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adminlogins`
+--
+
+INSERT INTO `adminlogins` (`id`, `admin_id`, `username`, `userpassword`) VALUES
+(1, 'admin-10001', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessments`
+--
+
+CREATE TABLE `assessments` (
+  `id` int(11) NOT NULL,
+  `assessmentTitle` varchar(30) DEFAULT NULL,
+  `assessmenttype` varchar(20) NOT NULL,
+  `quarterperiod` varchar(20) DEFAULT NULL,
+  `dateGiven` varchar(25) NOT NULL,
+  `studentID` varchar(25) DEFAULT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `subjectname` varchar(25) DEFAULT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `grade` int(10) DEFAULT NULL,
+  `total` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessmenttype`
+--
+
+CREATE TABLE `assessmenttype` (
+  `id` int(11) NOT NULL,
+  `assessmenttype` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `visibility` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `department`, `visibility`) VALUES
+(1, 'Araling Panlipunan', 'Visible');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `lessonID` int(11) DEFAULT NULL,
+  `lessonTitle` varchar(80) NOT NULL,
+  `subjectname` varchar(25) DEFAULT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `quarterperiod` varchar(20) DEFAULT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `section1` longtext DEFAULT NULL,
+  `section2` longtext DEFAULT NULL,
+  `section3` longtext DEFAULT NULL,
+  `section4` longtext DEFAULT NULL,
+  `section5` longtext DEFAULT NULL,
+  `section6` longtext DEFAULT NULL,
+  `section7` longtext DEFAULT NULL,
+  `section8` longtext DEFAULT NULL,
+  `section9` longtext DEFAULT NULL,
+  `section10` longtext DEFAULT NULL,
+  `dateCreated` datetime DEFAULT NULL,
+  `dateUpdated` datetime DEFAULT NULL,
+  `visibility` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quarters`
+--
+
+CREATE TABLE `quarters` (
+  `id` int(11) NOT NULL,
+  `quarterperiod` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE `sections` (
+  `id` int(11) NOT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `visibility` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentattendance`
+--
+
+CREATE TABLE `studentattendance` (
+  `id` int(11) NOT NULL,
+  `studentID` varchar(25) DEFAULT NULL,
+  `subjectname` varchar(25) DEFAULT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `date` varchar(25) NOT NULL,
+  `quarterperiod` varchar(20) DEFAULT NULL,
+  `attendance` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentlogins`
+--
+
+CREATE TABLE `studentlogins` (
+  `id` int(11) NOT NULL,
+  `studentID` varchar(25) DEFAULT NULL,
+  `studentUserName` varchar(25) DEFAULT NULL,
+  `studentPassword` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `studentID` varchar(25) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `middlename` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `suffix` varchar(15) DEFAULT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `dateEnrolled` datetime DEFAULT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjectoverview`
+--
+
+CREATE TABLE `subjectoverview` (
+  `id` int(11) NOT NULL,
+  `teacherid` varchar(25) NOT NULL,
+  `subjectname` varchar(25) NOT NULL,
+  `sectionname` varchar(50) NOT NULL,
+  `overview` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subjectid` varchar(25) DEFAULT NULL,
+  `subjectname` varchar(25) DEFAULT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `sectionname` varchar(50) DEFAULT NULL,
+  `visibility` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacherannouncements`
+--
+
+CREATE TABLE `teacherannouncements` (
+  `id` int(11) NOT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `subjectname` varchar(25) DEFAULT NULL,
+  `sectionname` varchar(25) DEFAULT NULL,
+  `announcementTitle` varchar(30) NOT NULL,
+  `announcement` longtext NOT NULL,
+  `visibility` varchar(15) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacherdetails`
+--
+
+CREATE TABLE `teacherdetails` (
+  `id` int(11) NOT NULL,
+  `teacherid` varchar(25) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `middlename` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `suffix` varchar(50) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL,
+  `visibility` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacherlogins`
+--
+
+CREATE TABLE `teacherlogins` (
+  `id` int(11) NOT NULL,
+  `teacherid` varchar(25) NOT NULL,
+  `userlogin` varchar(25) DEFAULT NULL,
+  `userpassword` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploaded_files`
+--
+
+CREATE TABLE `uploaded_files` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `sectionname` varchar(255) NOT NULL,
+  `name_of_file` varchar(255) NOT NULL,
+  `teacherid` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `mimetype` varchar(255) NOT NULL,
+  `size` int(255) NOT NULL,
+  `visibility` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admindetails`
+--
+ALTER TABLE `admindetails`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_id` (`admin_id`),
+  ADD KEY `department` (`department`);
+
+--
+-- Indexes for table `adminlogins`
+--
+ALTER TABLE `adminlogins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `assessments`
+--
+ALTER TABLE `assessments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `assessmenttype` (`assessmenttype`),
+  ADD KEY `quarterperiod` (`quarterperiod`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `sectionname` (`sectionname`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `teacherid` (`teacherid`);
+
+--
+-- Indexes for table `assessmenttype`
+--
+ALTER TABLE `assessmenttype`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `assessmenttype` (`assessmenttype`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `department` (`department`);
+
+--
+-- Indexes for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `quarterperiod` (`quarterperiod`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `quarters`
+--
+ALTER TABLE `quarters`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quarterperiod` (`quarterperiod`);
+
+--
+-- Indexes for table `sections`
+--
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `studentattendance`
+--
+ALTER TABLE `studentattendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `sectionname` (`sectionname`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `quarterperiod` (`quarterperiod`);
+
+--
+-- Indexes for table `studentlogins`
+--
+ALTER TABLE `studentlogins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studentID` (`studentID`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `subjectoverview`
+--
+ALTER TABLE `subjectoverview`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subjectid` (`subjectid`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `teacherannouncements`
+--
+ALTER TABLE `teacherannouncements`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `subjectname` (`subjectname`),
+  ADD KEY `sectionname` (`sectionname`);
+
+--
+-- Indexes for table `teacherdetails`
+--
+ALTER TABLE `teacherdetails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacherid` (`teacherid`),
+  ADD KEY `department` (`department`);
+
+--
+-- Indexes for table `teacherlogins`
+--
+ALTER TABLE `teacherlogins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacherid` (`teacherid`);
+
+--
+-- Indexes for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sectionname` (`sectionname`),
+  ADD KEY `teacherid` (`teacherid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admindetails`
+--
+ALTER TABLE `admindetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `adminlogins`
+--
+ALTER TABLE `adminlogins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `assessments`
+--
+ALTER TABLE `assessments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `assessmenttype`
+--
+ALTER TABLE `assessmenttype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quarters`
+--
+ALTER TABLE `quarters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sections`
+--
+ALTER TABLE `sections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `studentattendance`
+--
+ALTER TABLE `studentattendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `studentlogins`
+--
+ALTER TABLE `studentlogins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjectoverview`
+--
+ALTER TABLE `subjectoverview`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacherannouncements`
+--
+ALTER TABLE `teacherannouncements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacherdetails`
+--
+ALTER TABLE `teacherdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacherlogins`
+--
+ALTER TABLE `teacherlogins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `uploaded_files`
+--
+ALTER TABLE `uploaded_files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admindetails`
+--
+ALTER TABLE `admindetails`
+  ADD CONSTRAINT `admindetails_ibfk_1` FOREIGN KEY (`department`) REFERENCES `departments` (`department`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `adminlogins`
+--
+ALTER TABLE `adminlogins`
+  ADD CONSTRAINT `adminlogins_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admindetails` (`admin_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `assessments`
+--
+ALTER TABLE `assessments`
+  ADD CONSTRAINT `assessments_ibfk_1` FOREIGN KEY (`assessmenttype`) REFERENCES `assessmenttype` (`assessmenttype`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `assessments_ibfk_2` FOREIGN KEY (`quarterperiod`) REFERENCES `quarters` (`quarterperiod`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `assessments_ibfk_3` FOREIGN KEY (`studentID`) REFERENCES `students` (`studentID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `assessments_ibfk_4` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `assessments_ibfk_5` FOREIGN KEY (`subjectname`) REFERENCES `subjects` (`subjectname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `assessments_ibfk_6` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`subjectname`) REFERENCES `subjects` (`subjectname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `lessons_ibfk_2` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `lessons_ibfk_3` FOREIGN KEY (`quarterperiod`) REFERENCES `quarters` (`quarterperiod`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `lessons_ibfk_4` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `studentattendance`
+--
+ALTER TABLE `studentattendance`
+  ADD CONSTRAINT `studentattendance_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `students` (`studentID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `studentattendance_ibfk_2` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `studentattendance_ibfk_3` FOREIGN KEY (`quarterperiod`) REFERENCES `quarters` (`quarterperiod`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `studentattendance_ibfk_4` FOREIGN KEY (`subjectname`) REFERENCES `subjects` (`subjectname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `studentattendance_ibfk_5` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `studentlogins`
+--
+ALTER TABLE `studentlogins`
+  ADD CONSTRAINT `studentlogins_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `students` (`studentID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subjectoverview`
+--
+ALTER TABLE `subjectoverview`
+  ADD CONSTRAINT `subjectoverview_ibfk_1` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjectoverview_ibfk_2` FOREIGN KEY (`subjectname`) REFERENCES `subjects` (`subjectname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjectoverview_ibfk_3` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjects_ibfk_2` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teacherannouncements`
+--
+ALTER TABLE `teacherannouncements`
+  ADD CONSTRAINT `teacherannouncements_ibfk_1` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `teacherannouncements_ibfk_2` FOREIGN KEY (`subjectname`) REFERENCES `subjectoverview` (`subjectname`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `teacherannouncements_ibfk_3` FOREIGN KEY (`sectionname`) REFERENCES `sections` (`sectionname`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teacherdetails`
+--
+ALTER TABLE `teacherdetails`
+  ADD CONSTRAINT `teacherdetails_ibfk_1` FOREIGN KEY (`department`) REFERENCES `departments` (`department`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teacherlogins`
+--
+ALTER TABLE `teacherlogins`
+  ADD CONSTRAINT `teacherlogins_ibfk_1` FOREIGN KEY (`teacherid`) REFERENCES `teacherdetails` (`teacherid`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
